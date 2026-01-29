ea554dcb2fe98b817b4e4a7d81715806275c6c6b
Kaelyn <you@example.com>
2026-01-29 13:50:03 -0500

    lab 4

---
+++ guinnea_pig.py
@@ -0,0 +1,31 @@
+
+class GuinneaPig:
+    def __init__(self, name: str, color: str):
+        self.name = name
+        self.color = color
+        self.legs = 4
+
+    def squeak(self):
+        return f'{self.name} the {self.color} guinnea pig squeaks!'
+    
+if __name__ == '__main__':
+    # print('new dawn, new day')
+    pipsqueak = GuinneaPig('Pipsqueak', 'calico')
+    print(pipsqueak.squeak())
+
+class GuinneaPig: 
+    def __init__(self, name: str, color: str):
+        self.name = name 
+        self.color = color 
+        self.legs = 2
+
+    def squeak(self):
+        return f'{self.name} the {self.color} guinnea pig squeaks!'
+        
+if __name__ == '__main__':
+    #print('new dawn, new day')
+    potato = GuinneaPig('Potato', 'brown')
+    print(potato.squeak()) 
+
+
+        
--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-
-
-if __name__ == '__main__':
-    print('new dawn, new day')
+class GuinneaPig: 
+     def __init__(self, name: str, color: str):
+        self.name = name 
+        self.color = color 
+        self.eyes = 2
+        def squeak(self):
+            return f' {self.name} the [self.color] guinnea pig squeaks!'
+        if __name__ == '__main__':
+            #print('new dawn, new day')
+            potato = GuinneaPig('Potato', 'brown')
+            print(potato.squeak())
+            
