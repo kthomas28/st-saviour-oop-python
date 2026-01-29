7906b614fe32e8119f20ca3f8d9dc31aeccdab01
Kaelyn <you@example.com>
2026-01-29 14:41:34 -0500

    lab 4

--- guinnea_pig.py
+++ guinnea_pig.py
@@ -13,17 +13,6 @@ if __name__ == '__main__':
     pipsqueak = GuinneaPig('Pipsqueak', 'calico')
     print(pipsqueak.squeak())
 
-class GuinneaPig: 
-    def __init__(self, name: str, color: str):
-        self.name = name 
-        self.color = color 
-        self.legs = 2
-
-    def squeak(self):
-        return f'{self.name} the {self.color} guinnea pig squeaks!'
-        
-if __name__ == '__main__':
-    #print('new dawn, new day')
     potato = GuinneaPig('Potato', 'brown')
     print(potato.squeak()) 
 
